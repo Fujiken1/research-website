cd1941a7b55ed6b0cb17cb64d8b5ed5559fc6db3
Fujiken1 <123650729+Fujiken1@users.noreply.github.com>
2023-02-25 19:03:30 +0800

    research purposes

---
+++ inkwiris1.js
@@ -0,0 +1,21 @@
+function changeParagraphContent() {
+    document.getElementById("paragraph1").innerHTML = "Adobo is a popular Filipino dish that consists of marinated meat (usually pork or chicken) cooked in a sauce made from vinegar, soy sauce, and spices. Here is a simple adobo recipe: <ul>Ingredients:<li>1 pound pork or chicken, cut into bite-sized pieces</li><li>1/2 cup white vinegar</li><li>1/2 cup soy sauce</li><li>1/2 cup water</li><li>1 head garlic, minced</li><li>1 teaspoon black pepper</li><li>3-4 bay leaves</li><li>2 tablespoons cooking oil</li><ul>Instructions:</ul> <ul>1. In a large bowl, combine the vinegar, soy sauce, water, garlic, black pepper, and bay leaves.</ul><ul>2. Add the pork or chicken to the marinade and mix well, making sure that all the meat is coated. Marinate for at least 30 minutes, or up to overnight in the refrigerator.</ul><ul>3. Heat the cooking oil in a large skillet or pot over medium-high heat. Remove the meat from the marinade (reserve the marinade for later) and cook until browned on all sides, about 5 minutes.</ul><ul>4. Pour the reserved marinade into the skillet or pot with the meat and bring to a boil.</ul><ul>5. Lower the heat and let the meat simmer in the sauce for 30-40 minutes, or until the meat is tender and the sauce has thickened.</ul><ul>6.Serve hot with steamed rice.</ul> "
+    
+}
+
+function changeParagraphContent1(){
+    document.getElementById("paragraph1").innerHTML = "This is a new text"
+}
+
+function changeParagraphContent2() {
+    document.getElementById("paragraph1").innerHTML =" This is a new Paragraph"
+}
+
+function changeParagraphContent3() {
+    document.getElementById("paragraph1").innerHTML =" This is a new sentence"
+}
+
+function changeParagraphContent4() {
+    document.getElementById("paragraph1").innerHTML = "<strong>Philippine food</strong> is a diverse and flavorful cuisine that reflects the country's history and geography. The Philippines is an archipelago of over 7,000 islands, and its food culture has been influenced by many factors, including Spanish, Chinese, and Malay cuisines. The result is a unique and delicious blend of flavors and ingredients. One of the most popular dishes in the Philippines is adobo, a savory stew made with meat (usually chicken or pork), soy sauce, vinegar, garlic, and other spices. Adobo is often served with rice and is a staple in many Filipino households. Another popular dish is sinigang, a sour soup made with tamarind, vegetables, and meat or seafood. Filipinos also enjoy a variety of grilled and roasted meats, such as lechon (roast pig) and inihaw na baboy (grilled pork). Seafood is also a big part of Philippine cuisine, with dishes like grilled squid, crispy fried fish, and kinilaw (raw fish marinated in vinegar). Snacks and street food are also an important part of Philippine food culture. Some popular snacks include lumpia (spring rolls), kwek-kwek (deep-fried quail eggs), and balut (boiled fertilized duck egg). In conclusion, Philippine food is a delicious and diverse cuisine that reflects the country's rich history and cultural influences. From savory stews to grilled meats and street food snacks, Philippine food offers a unique and satisfying culinary experience that should not be missed."
+    
+}
